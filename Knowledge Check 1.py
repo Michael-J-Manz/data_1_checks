0d91b42140b0dec783da233877b0a14e90dacd83
Michael-Manz <pastormanz@gmail.com>
2022-10-02 13:52:18 -0400

    First commit

---
+++ Knowledge Check 1.py
@@ -0,0 +1,13 @@
+# To complete the first knowledge check, do the following: Create a GitHub repo called "data_1_checks".
+# Commit your changes. Push your changes to your repo and notify your mentor!
+
+print("hello world!")
+
+sample_list = ["Eggs", 16, True, 3.1415]
+print(sample_list[1])
+
+example_dictionary = {"title":"Old Man and the Sea", "author":"Ernest Hemingway"}
+print(example_dictionary["author"])
+
+prototype = [("Mary", "07"), ("Michal", "'10"), ("John", "'12"), ("Shirley", "'13")]
+print(prototype[-2])
